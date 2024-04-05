a615acc66999ccc9dd0ca106bbe8e73dc4e52882
Meenakshi A.M. <miniaalayam@gmail.com>
2024-04-05 09:04:55 +0530

    remove duplicates method-1

---
+++ arrays/removeDuplicate.java
@@ -0,0 +1,16 @@
+public class removeDuplicate {
+    public static void main(String[] args) {
+        
+    }
+    int remove_duplicate(int arr[],int N){
+        // code here
+        int i = 0;// pointer 1 which tracks the indices
+        for(int j = 1; j < N; j++){
+            if(arr[i] != arr[j]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}
