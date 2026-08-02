bc674672134431d5776f38d4a678fd4f9f0662fc
Meenakshi A.M. <140526841+MeenakshiAM@users.noreply.github.com>
2026-08-02 09:56:38 +0530

    Implement countRatioSubarrays method in Solution class

---
+++ leetcode/Aug-2026/contest-1/Q2. Count Subarrays With Even Odd Ratio I/soln.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a, int b) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int even = 0, odd = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) even++;
+                else odd++;
+
+                if (odd > 0 && even * b <= odd * a) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
